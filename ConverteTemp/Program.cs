b27635c7e11010c72971833f78738867f7a1941a
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:32:54 -0300

    Add project files.

---
+++ ConverteTemp/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float temp;
+
+        Console.WriteLine("Digite a temp em ºC: ");
+        temp = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("A temperatura em ºF é: " + ((temp -32)*5/9).ToString("F2"));
+    }
+}
